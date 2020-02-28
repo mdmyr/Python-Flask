76d52eedea78a97f29eb1a0539d4284fce579f94
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2020-02-28 15:01:17 -0500

    commit the file chagnes as it is working :wq

--- Day2.py
+++ Day2.py
@@ -5,14 +5,26 @@ from printthis import printthis
 
 
 app=Flask(__name__)
+print('OUT SID THE MAIN')
 printthis(dir(app))
 
+
+
+@app.route('/home')
+def home():
+    return 'You are home'
+
+
+
+def helloWorld():
+    return 'Hello World'
+app.add_url_rule('/','hello',helloWorld)
+
+
+
 @app.route('/')
-
-
 def main():
-    return 'This is a  simple return'
-    prinththis(print(dir(app)))
-
+   # return 'This is a  simple return'
+    return "List"
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
